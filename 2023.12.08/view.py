fad9ad5d44125993606cdd9eb2a338bbfa2684f7
kfesaxd <Heffman127@gmail.com>
2023-12-13 21:29:03 +0300

    add view.py

---
+++ 2023.12.08/view.py
@@ -0,0 +1,15 @@
+class CLI:
+    def start_email():
+        print("Начало работы программы".center(35, "-"))
+
+    def get_email():
+        return input("\nВведите email: ")
+    
+    def invalid_email():
+        print("\nВведен некорректный emali") 
+    
+    def save_email():
+        print("\nEmail успешно сохранен в файл") 
+    
+    def end_email():
+        print("Конец работы программы".center(35, "-")) 
